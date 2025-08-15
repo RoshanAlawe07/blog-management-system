74857b079786660c78b2615ea4d3a17e9c6824e7
Roshanalawe <141437132+Strsnger07@users.noreply.github.com>
2025-08-15 22:05:44 +0530

    some code change

--- app/admin/addBlog/page.jsx
+++ app/admin/addBlog/page.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 
-const page = () => {
+const Page = () => {
 
     const [image,setImage] = useState(false);
     const [data,setData] = useState({
@@ -54,7 +54,7 @@ const page = () => {
       <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
         <p className='text-xl'>Upload thumbnail</p>
         <label htmlFor="image">
-            <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt=''/>
+            <Image className='mt-4' src={!image?assets.upload_area:URL.createObjectURL(image)} width={140} height={70} alt='Upload thumbnail'/>
         </label>
         <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required />
         <p className='text-xl mt-4'>Blog title</p>
@@ -74,4 +74,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify';
 
-const page = () => {
+const Page = () => {
 
   const [emails,setEmails] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -99,4 +99,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Page
