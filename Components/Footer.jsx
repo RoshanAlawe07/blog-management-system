8c2dd52d55cc705c459b7f815bef771fc0198743
Roshanalawe <141437132+Strsnger07@users.noreply.github.com>
2025-08-18 22:41:45 +0530

    updates

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
       <Image src={assets.logo_light} alt='' width={120} height={40} />
-      <p className='text-sm text-white'>All right reserved. Copyright @blogger</p>
+      <p className='text-sm text-white'>Allll right reserved. Copyright @blogger</p>
         <div className='flex'>
             <Image src={assets.facebook_icon} alt='' width={40} height={40} />
             <Image src={assets.twitter_icon} alt='' width={40} height={40} />
