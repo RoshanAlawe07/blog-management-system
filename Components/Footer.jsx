4c5699e94f69597bd172d394bb7a2d2c7c4442d6
Roshanalawe <141437132+Strsnger07@users.noreply.github.com>
2025-08-19 02:30:57 +0530

    footerr error

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -4,14 +4,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
-      <Image src={assets.logo_light} alt='' width={120} height={40} />
-      <p className='text-sm text-white'>All right reserved. Copyright @blogger</p>
-        <div className='flex'>
-            <Image src={assets.facebook_icon} alt='' width={40} height={40} />
-            <Image src={assets.twitter_icon} alt='' width={40} height={40} />
-            <Image src={assets.googleplus_icon} alt='' width={40} height={40} />
+    <div className='bg-black py-4 px-4'>
+      <div className='max-w-7xl mx-auto flex flex-col sm:flex-row justify-between items-center gap-8'>
+        {/* Logo */}
+        <div className='flex-shrink-0'>
+          <Image src={assets.logo_light} alt='Blog Logo' width={160} height={60} className='w-32 sm:w-40' />
         </div>
+        
+        {/* Copyright Text */}
+        <div className='text-center sm:text-left flex-shrink-0'>
+          <p className='text-base text-white font-semibold'>All rights reserved. Copyright © 2024 Blogger</p>
+        </div>
+        
+        {/* Social Media Icons */}
+        <div className='flex gap-3 flex-shrink-0'>
+          <Image src={assets.facebook_icon} alt='Facebook' width={24} height={24} className='w-6 h-6 hover:opacity-80 transition-opacity cursor-pointer' />
+          <Image src={assets.twitter_icon} alt='Twitter' width={24} height={24} className='w-6 h-6 hover:opacity-80 transition-opacity cursor-pointer' />
+          <Image src={assets.googleplus_icon} alt='Google Plus' width={24} height={24} className='w-6 h-6 hover:opacity-80 transition-opacity cursor-pointer' />
+        </div>
+      </div>
     </div>
   )
 }
